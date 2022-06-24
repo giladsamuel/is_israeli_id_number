15fd95218091357b1d4c9813c68ff7f1d468a2d3
Gilad Samuel <giladsam98@gmail.com>
2022-06-24 13:42:59 +0300

    first virsion

---
+++ app.py
@@ -0,0 +1,48 @@
+# This program check is a given ID is valid number by the "check digit" system
+# 
+# @author: Gilad Samuel.
+# @version: 1.0.0
+
+def sum_digits(n):                                      # return the sum of digits of a number
+    s = 0
+    while n:
+        s += n % 10
+        n //= 10
+    return s
+
+check_list = [1,2,1,2,1,2,1,2,1]
+check_sum = 0
+id_list = []
+
+ID = input("please type your ID: ")
+
+if len(ID) != 9 :                                        # ID don't have 9 digits
+    print("ID shold be 9 digits")
+    
+elif not ID.isdigit():                                   # ID isn't a number
+    print("Illegal ID, all digits need to be numbers")
+    
+else: 
+    ID = int(ID)                                          # change ID varibel to be int from str
+
+    while ID > 0:                                         # put all digits of ID in a list
+        id_list.insert(0, ID % 10)
+        ID //=10
+
+    for x in range(9):                                    # multiply ID digits with check_list digits
+        temp = check_list[x] * id_list[x] 
+        
+        if temp <=9:                                      # if result of multiply greater then 9, sum of digits indted
+            check_list[x] = temp                          
+        else:
+             check_list[x] = sum_digits(temp)             
+    
+    for x in check_list:                                  #sum results
+        check_sum += x
+
+    if check_sum % 10 == 0:                               # if sum divided by 10 with no remainder, ID valid
+        print("ID number is valid")        
+    else: 
+        print("ID number is not valid")
+
+
